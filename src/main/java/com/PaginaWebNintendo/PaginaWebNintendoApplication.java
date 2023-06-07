74167e5f6e110d9581916447299dd138a022d882
Felipe Chaves <84556078+felipe-chaves15@users.noreply.github.com>
2023-06-07 11:31:30 -0600

    taraa 1 prueba

---
+++ src/main/java/com/PaginaWebNintendo/PaginaWebNintendoApplication.java
@@ -0,0 +1,13 @@
+package com.PaginaWebNintendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebNintendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebNintendoApplication.class, args);
+	}
+
+}
